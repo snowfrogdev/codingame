ed73b5220981f862ea9201be24858bff6487c157
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-04-26 10:42:40 -0400

    Add solution to Lumen puzzle

--- lumen/dist/lumen.umd.js
+++ lumen/dist/lumen.umd.js
@@ -8,23 +8,86 @@
     var room = [];
     for (var i = 0; i < ROOM_SIZE; i++) {
         var LINE = readline();
-        // TODO: Remove spaces from LINE
-        room.push();
+        room.push(LINE.replace(/\s/g, ''));
     }
-    console.log(room);
+    var mappedRoom = room.map(function (row, i) {
+        var mappedRow = [];
+        for (var j = 0; j < ROOM_SIZE; j++) {
+            if (row.charAt(j) === 'C') {
+                mappedRow.push(LIGHT_STRENGTH);
+            }
+            else {
+                mappedRow.push(0);
+            }
+        }
+        return mappedRow;
+    });
+    var Position = /** @class */ (function () {
+        function Position(x, y) {
+            this.x = x;
+            this.y = y;
+        }
+        return Position;
+    }());
+    var findAdjacentSpotFunctions = [
+        function (x, y) { return new Position(x, y - 1); },
+        function (x, y) { return new Position(x + 1, y - 1); },
+        function (x, y) { return new Position(x + 1, y); },
+        function (x, y) { return new Position(x + 1, y + 1); },
+        function (x, y) { return new Position(x, y + 1); },
+        function (x, y) { return new Position(x - 1, y + 1); },
+        function (x, y) { return new Position(x - 1, y); },
+        function (x, y) { return new Position(x - 1, y - 1); },
+    ];
+    function mapRoom(lightStrength) {
+        if (!lightStrength) {
+            return;
+        }
+        mappedRoom.forEach(function (row, i) {
+            var _loop_1 = function (j) {
+                if (row[j] === lightStrength) {
+                    findAdjacentSpotFunctions.forEach(function (findAdjacentSpot) {
+                        var spot = findAdjacentSpot(j, i);
+                        if (spot.y >= 0 && spot.x >= 0 && spot.y < ROOM_SIZE && spot.x < ROOM_SIZE) {
+                            var adjacentSpot = mappedRoom[spot.y][spot.x];
+                            if (adjacentSpot < lightStrength - 1)
+                                mappedRoom[spot.y][spot.x] = lightStrength - 1;
+                        }
+                    });
+                }
+            };
+            for (var j = 0; j < ROOM_SIZE; j++) {
+                _loop_1(j);
+            }
+        });
+        mapRoom(lightStrength - 1);
+    }
+    mapRoom(LIGHT_STRENGTH);
+    var answer = mappedRoom.reduce(function (count, row) {
+        for (var _i = 0, row_1 = row; _i < row_1.length; _i++) {
+            var spot = row_1[_i];
+            if (spot === 0) {
+                count += 1;
+            }
+        }
+        return count;
+    }, 0);
+    console.log(answer);
     /*
     Example for the light spread N = 5, L = 3:
+    5
+    3
+    C X X X C
     X X X X X
-    X C X X X
     X X X X X
     X X X X X
-    X X X X X
+    C X X X C
 
-    2 2 2 1 0
-    2 3 2 1 0
-    2 2 2 1 0
-    1 1 1 1 0
-    0 0 0 0 0
+    3 2 1 2 3
+    2 2 1 2 2
+    1 1 1 1 1
+    2 2 1 2 2
+    3 2 1 2 3
     */
 
 }));
--- lumen/src/main.ts
+++ lumen/src/main.ts
@@ -1,35 +1,93 @@
 declare function readline(): string
 
-const ROOM_SIZE = parseInt(readline());
-const LIGHT_STRENGTH = parseInt(readline());
+const ROOM_SIZE = parseInt(readline())
+const LIGHT_STRENGTH = parseInt(readline())
 
 const room: string[] = []
 
 for (let i = 0; i < ROOM_SIZE; i++) {
     const LINE = readline()
-    // TODO: Remove spaces from LINE
-    room.push()
+    room.push(LINE.replace(/\s/g, ''))
 }
 
-const mappedRoom = room.map((row, i) => {   
-    for (let j = 0; j < ROOM_SIZE; j++) {
-           // console.error(row.charAt(j))
+const mappedRoom = room.map((row, i) => {
+    let mappedRow: number[] = []   
+    for (let j = 0; j < ROOM_SIZE; j++) {        
+           if (row.charAt(j) === 'C') {
+               mappedRow.push(LIGHT_STRENGTH)
+           } else {
+               mappedRow.push(0)
+           }
+           
     }
+    return mappedRow
 })
 
-console.log(room);
+class Position {
+    constructor(public x: number, public y: number) {}
+}
+
+const findAdjacentSpotFunctions = [
+    (x: number, y: number) => new Position(x, y - 1),
+    (x: number, y: number) => new Position(x + 1, y - 1),
+    (x: number, y: number) => new Position(x + 1, y),
+    (x: number, y: number) => new Position(x + 1, y + 1),
+    (x: number, y: number) => new Position(x, y + 1),
+    (x: number, y: number) => new Position(x - 1, y + 1),
+    (x: number, y: number) => new Position(x - 1, y),
+    (x: number, y: number) => new Position(x - 1, y - 1),
+]
+
+function mapRoom(lightStrength: number) {
+    if (!lightStrength) {
+        return
+    }
+
+    mappedRoom.forEach((row, i) => {
+        for (let j = 0; j < ROOM_SIZE; j++) {
+            if (row[j] === lightStrength) {
+                findAdjacentSpotFunctions.forEach(findAdjacentSpot => {
+                    const spot = findAdjacentSpot(j, i)
+                    if (spot.y >=0 && spot.x >= 0 && spot.y < ROOM_SIZE && spot.x < ROOM_SIZE) {                        
+                        const adjacentSpot = mappedRoom[spot.y][spot.x]
+                        if (adjacentSpot < lightStrength - 1)
+                        mappedRoom[spot.y][spot.x] = lightStrength - 1 
+                    }
+                })
+            }
+        }
+    })
+    
+    mapRoom(lightStrength - 1)
+}
+
+mapRoom(LIGHT_STRENGTH)
+
+const answer = mappedRoom.reduce((count, row) => {
+    for (const spot of row) {
+        if (spot === 0) {
+            count += 1
+        }
+    }
+    return count
+}, 0)
+
+
+console.log(answer)
 
 /*
 Example for the light spread N = 5, L = 3:
+5
+3
+C X X X C
 X X X X X
-X C X X X
 X X X X X
 X X X X X
-X X X X X
+C X X X C
 
-2 2 2 1 0
-2 3 2 1 0
-2 2 2 1 0
-1 1 1 1 0
-0 0 0 0 0
+3 2 1 2 3
+2 2 1 2 2
+1 1 1 1 1
+2 2 1 2 2
+3 2 1 2 3
 */
