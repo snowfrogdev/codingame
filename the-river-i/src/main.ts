75240ed5f700862915786a0099e8a47a18b66a1e
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-05-06 21:56:20 -0400

    add The Rive I. puzzle

---
+++ the-river-i/src/main.ts
@@ -0,0 +1,12 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+const r1 = parseInt(readline());
+const r2 = parseInt(readline());
+
+// Write an action using console.log()
+// To debug: console.error('Debug messages...');
+
+console.log('42');
