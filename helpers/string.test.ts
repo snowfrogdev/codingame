32e360c024537aca593f7c4e9595bbe918ea382f
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-05-19 21:27:55 -0400

    Extend String into a new class MyString
    The `readline` function allows the reading of a string one line at a time. This can be useful for testing and emulating receiving lines of text from stdin.

---
+++ helpers/string.test.ts
@@ -0,0 +1,18 @@
+import { MyString } from "./string";
+
+describe('MyString', () => {
+    test('readline works', () => {
+        const input = 'This is the first line\n' +
+        'This is the second line\n' +
+        'This is the third line'
+        const str = new MyString(input)
+
+        const firstCall = str.readline()
+        const secondCall = str.readline()
+        const thirdCall = str.readline()
+
+        expect(firstCall).toBe('This is the first line')
+        expect(secondCall).toBe('This is the second line')
+        expect(thirdCall).toBe('This is the third line')
+    })
+})
