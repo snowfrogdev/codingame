79b118cdf7c2e9a7e6885d0da5eaed6da1e8efc4
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-05-23 11:26:49 -0400

    Pass Height 16 & Width -2 in Seam Carving

--- seam-carving/src/pgm-image.ts
+++ seam-carving/src/pgm-image.ts
@@ -1,6 +1,7 @@
 class Pixel {
     private energy_: number = 0
-    private cumulativeEnergy_: number = 0;
+    private cumulativeEnergy_: number = 0
+    private _parentPixelColumnIndex_: number = 0;
 
     constructor(readonly grayscale: number) { }
 
@@ -16,39 +17,79 @@ class Pixel {
     set cumulativeEnergy(value: number) {
         this.cumulativeEnergy_ = value;
     }
+    get parentPixelColumnIndex_(): number {
+        return this._parentPixelColumnIndex_;
+    }
+    set parentPixelColumnIndex_(value: number) {
+        this._parentPixelColumnIndex_ = value;
+    }
 }
 
 export class PgmImage {
+    private seams_: number[][] = []
     constructor(private imageFile_: string) { }
 
-    removeSeam(): string {
-        const pixels = this.imageFile_.split('\n').slice(4).map(row => row.split(' ').map(grayscale => new Pixel(Number(grayscale))))
+    removeSeams(): string {
+        let output = ''
+
+        const lines = this.imageFile_.split('\n')
+        const [W, H] = lines[1].split(' ').map(Number)
+        const seamsToBeRemoved = W - parseInt(lines[2].split(' ')[1])
+        const pixels = lines.slice(4).map(row => row.split(' ').map(grayscale => new Pixel(Number(grayscale))))
 
         if (pixels.length < 3 && pixels[0].length < 3) {
             return '0'
         }
 
-        this.computeGradientMagnitude_(pixels);
+        for (let i = 0; i < seamsToBeRemoved; i++) {
+            this.computeGradientMagnitude_(pixels)
+            this.computeCumulativeEnergy_(pixels)
 
-        this.computeCumulativeEnergy_(pixels);
+            const pathOfLeastEnergy = pixels[pixels.length - 1].reduce((p, c, x, array) => {
+                const current = { xIndex: x, cumulativeEnergy: c.cumulativeEnergy }
+                const sortedArray = [p, current].sort((a, b) => a.cumulativeEnergy - b.cumulativeEnergy)
+                return { xIndex: sortedArray[0].xIndex, cumulativeEnergy: sortedArray[0].cumulativeEnergy }
+            }, { xIndex: 0, cumulativeEnergy: Infinity })
 
-        const pathOfLeastEnergy = pixels[pixels.length - 1].reduce((p, c, x, array) => {
-            /*if (x === 0 || x === array.length -1) {
-                return p
-            }*/
-            return Math.min(p, c.cumulativeEnergy)
-        }, Infinity)
+            output += pathOfLeastEnergy.cumulativeEnergy.toString() + '\n'
 
-        return pathOfLeastEnergy.toString()
+            let currentIndexToRemove = pathOfLeastEnergy.xIndex
+            let nextXIndexToRemove;
+            for (let y = pixels.length - 1; y >= 0; y--) {
+                nextXIndexToRemove = pixels[y][currentIndexToRemove].parentPixelColumnIndex_
+                pixels[y].splice(currentIndexToRemove, 1)
+                currentIndexToRemove = nextXIndexToRemove
+            }
+        }
+
+        return output.trim()
     }
 
     private computeCumulativeEnergy_(pixels: Pixel[][]) {
         pixels.forEach((row, y, array) => {
+            const seam: number[] = []
             return row.forEach((pixel, x) => {
                 if (y === 0) {
                     pixel.cumulativeEnergy = pixel.energy
                     return
                 }
+
+                const northWest = { xIndex: x - 1, cumulativeEnergy: Infinity }
+                const northEast = { xIndex: x + 1, cumulativeEnergy: Infinity }
+                const north = { xIndex: x, cumulativeEnergy: Infinity }
+                if (x !== 0) {
+                    northWest.cumulativeEnergy = array[y - 1][northWest.xIndex].cumulativeEnergy
+                }
+                if (x !== row.length - 1) {
+                    northEast.cumulativeEnergy = array[y - 1][northEast.xIndex].cumulativeEnergy
+                }
+                north.cumulativeEnergy = array[y - 1][north.xIndex].cumulativeEnergy
+
+                const sortedArray = [northWest, northEast, north].sort((a, b) => a.cumulativeEnergy - b.cumulativeEnergy)
+                pixel.cumulativeEnergy = sortedArray[0].cumulativeEnergy + pixel.energy
+                pixel.parentPixelColumnIndex_ = sortedArray[0].xIndex
+
+                /*
                 let northWest = Infinity
                 let northEast = Infinity
                 if (x !== 0) {
@@ -59,6 +100,7 @@ export class PgmImage {
                 }
                 const north = array[y - 1][x].cumulativeEnergy;
                 pixel.cumulativeEnergy = Math.min(northWest, north, northEast) + pixel.energy
+                */
             })
         })
     }
