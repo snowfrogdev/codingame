d8d85eab4f9eb2d705b96af403b2692125f02789
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-05-29 10:42:46 -0400

    Pass all tests on rearrange-string-to-two-numbers

--- rearrange-string-to-two-numbers/src/rearrange-string.test.ts
+++ rearrange-string-to-two-numbers/src/rearrange-string.test.ts
@@ -30,4 +30,54 @@ describe('Rearrange string to two numbers', () => {
 
         expect(actual).toBe(expected)
     })
+
+    test('Too many 0s', () => {
+        const input = '0000000000100000000000'
+
+        const actual = rearrangeString(input)
+
+        const expected = '-1 -1'
+
+        expect(actual).toBe(expected)
+    })
+
+    test('Maximum B with 0', () => {
+        const input = '10000000000000000000'
+
+        const actual = rearrangeString(input)
+
+        const expected = '0 1000000000000000000'
+
+        expect(actual).toBe(expected)
+    })
+
+    test('Small A big B', () => {
+        const input = '79380248390522737902'
+
+        const actual = rearrangeString(input)
+
+        const expected = '20 200223334577788999'
+
+        expect(actual).toBe(expected)
+    })
+
+    test('Too few digits', () => {
+        const input = '9'
+
+        const actual = rearrangeString(input)
+
+        const expected = '-1 -1'
+
+        expect(actual).toBe(expected)
+    })
+
+    test('Zero', () => {
+        const input = '9407809450087866606'
+
+        const actual = rearrangeString(input)
+
+        const expected = '0 400004566667788899'
+
+        expect(actual).toBe(expected)
+    })
 })
--- rearrange-string-to-two-numbers/src/rearrange-string.ts
+++ rearrange-string-to-two-numbers/src/rearrange-string.ts
@@ -1,40 +1,50 @@
 export function rearrangeString(input: string): string {
     const allDigits = Array.from(input)
+    
+    // Check that we have enough digits
+    if (allDigits.length <= 1) {
+        return '-1 -1'
+    }
+
     const zeroes = allDigits.filter(digit => digit === '0')
     const otherDigits = allDigits.filter(digit => digit !== '0').sort()
-    let sizeOfA = 0
+    let remainingSpaceInA = 0
 
     let b = ''
     // Check if B can have 19 digits    
     if (otherDigits[0] === '1' && zeroes.length >= 18) {
         b = (10 ** 18).toString()
-        sizeOfA = allDigits.length - 19
+        zeroes.splice(0, 18)
+        otherDigits.shift()
+        remainingSpaceInA = allDigits.length - 19
     } else {
-        sizeOfA = allDigits.length - 18 >= 1 ? allDigits.length - 18 : 1
+        remainingSpaceInA = allDigits.length - 18 >= 1 ? allDigits.length - 18 : 1
     }
 
     let a = ''
     // Minimize A
     // First digit is the smallest number bigger than 0
-    a += otherDigits.shift()
-
-    // Shove as many zeroes as possible in a without busting sizeOfA - 1
-    if (zeroes.length <= sizeOfA - 1 && zeroes.length > 0) {
-        a += zeroes.splice(0, sizeOfA - 1).join('')
+    if (remainingSpaceInA === 1 && zeroes.length >= 1) {
+        ({ a, remainingSpaceInA } = addZeroesToA(zeroes, a, remainingSpaceInA))
+    } else if (otherDigits.length > 0) {
+        a += otherDigits.shift()
+        remainingSpaceInA -= 1
     }
 
+    // Shove as many zeroes as possible in a without busting remainingSpaceInA
+    ({ a, remainingSpaceInA } = addZeroesToA(zeroes, a, remainingSpaceInA))
+
     // Fill remaining space of a with the smallest numbers in digits
-    const remainingSpaceInA = sizeOfA - a.length
     a += otherDigits.splice(0, remainingSpaceInA).join('')
 
     // Minimize B
     // First digit is the smallest number bigger than 0
-    b += otherDigits.shift()
+    if (otherDigits.length > 0) {
+        b += otherDigits.shift()
+    }
 
     // Shove as many zeroes as possible in b
-    if (zeroes.length > 0) {
-        b += zeroes.splice(0, sizeOfA - 1).join('')
-    }
+        b += zeroes.join('')
 
     // Fill the rest b with the smallest numbers in digits
     b += otherDigits.join('')
@@ -44,8 +54,22 @@ export function rearrangeString(input: string): string {
         return '-1 -1'
     }
 
+    // Check that we don't have a bunch of zeroes
+    if ((a[0] === '0' && a[1] === '0') || (b[0] === '0' && b[1] === '0')) {
+        return '-1 -1'
+    }
+
     return `${a} ${b}`
 }
 
 
+
+function addZeroesToA(zeroes: string[], a: string, remainingSpaceInA: number) {
+    const zeroesRemaining = zeroes.length
+    if (zeroesRemaining > 0) {
+        a += zeroes.splice(0, remainingSpaceInA).join('')
+        remainingSpaceInA -= Math.min(remainingSpaceInA, zeroesRemaining)
+    }
+    return { a, remainingSpaceInA }
+}
 
