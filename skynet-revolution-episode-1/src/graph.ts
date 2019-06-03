5d362ec4eeeba2e7dcc143050d1457a263c34033
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-06-03 13:28:39 -0400

    Refactor skynet-revolution-episode-1

--- skynet-revolution-episode-1/src/graph.ts
+++ skynet-revolution-episode-1/src/graph.ts
@@ -1,122 +1,114 @@
 class Node {
-    private isMarked_ = false
-    private isGateway_ = false
-    private previousNode_: Node | undefined
-    private connectNodes_: Node[] = []
-    constructor(private index_: number) {}
+  private isMarked_ = false;
+  private isGateway_ = false;
+  private previousNode_: Node | undefined;
+  private connectNodes_: Node[] = [];
+  constructor(private index_: number) {}
 
-    get index() {
-        return this.index_
-    }
+  get index() {
+    return this.index_;
+  }
 
-    get isGateway() {
-        return this.isGateway_
-    }
-    set isGateway(value: boolean) {
-        this.isGateway_ = value
-    }
+  get isGateway() {
+    return this.isGateway_;
+  }
+  set isGateway(value: boolean) {
+    this.isGateway_ = value;
+  }
 
-    get previousNode(): Node | undefined {
-        return this.previousNode_
-    }
-    set previousNode(node: Node | undefined) {
-        this.previousNode_ = node
-    }
-    
-    get isMarked() {
-        return this.isMarked_
-    }
-    set isMarked(value: boolean) {
-        this.isMarked_ = value
-    }
+  get previousNode(): Node | undefined {
+    return this.previousNode_;
+  }
+  set previousNode(node: Node | undefined) {
+    this.previousNode_ = node;
+  }
 
-    
+  get isMarked() {
+    return this.isMarked_;
+  }
+  set isMarked(value: boolean) {
+    this.isMarked_ = value;
+  }
 
-    getConnectNodes() {
-        return this.connectNodes_
-    }
+  getConnectNodes() {
+    return this.connectNodes_;
+  }
 
-    addConnectedNode(connectNode: Node) {
-        this.connectNodes_.push(connectNode)
-    }
+  addConnectedNode(connectNode: Node) {
+    this.connectNodes_.push(connectNode);
+  }
 
-    removeConnectedNode(node: Node) {
-        const nodeIndex = this.connectNodes_.indexOf(node)
-        this.connectNodes_.splice(nodeIndex, 1)
-    }
+  removeConnectedNode(node: Node) {
+    const nodeIndex = this.connectNodes_.indexOf(node);
+    this.connectNodes_.splice(nodeIndex, 1);
+  }
 }
 
 export class Graph {
-    private nodes_: Node[]
-    constructor(numberOfNodes: number) {
-        this.nodes_ = Array.from(Array(numberOfNodes), (_, index) => new Node(index))
+  private nodes_: Node[];
+  constructor(numberOfNodes: number) {
+    this.nodes_ = Array.from(
+      Array(numberOfNodes),
+      (_, index) => new Node(index)
+    );
+  }
+
+  addLink(node1: number, node2: number) {
+    this.nodes_[node1].addConnectedNode(this.nodes_[node2]);
+    this.nodes_[node2].addConnectedNode(this.nodes_[node1]);
+  }
+
+  getNode(index: number) {
+    return this.nodes_[index];
+  }
+
+  removeLink(node1: number, node2: number) {
+    this.nodes_[node1].removeConnectedNode(this.nodes_[node2]);
+    this.nodes_[node2].removeConnectedNode(this.nodes_[node1]);
+  }
+
+  findBestLinkToRemove(agentsNodeIndex: number): [number, number] {
+    this.cleanGraph_();
+
+    const startingNode = this.nodes_[agentsNodeIndex];
+    const queue: Node[] = [startingNode];
+    startingNode.isMarked = true;
+    while (queue.length > 0) {
+      const currentNode = queue.shift() as Node;
+      if (currentNode.isGateway) {
+        const path = this.recursePath_(currentNode);
+        const secondNodeInPathIndex = path[path.length - 2];
+        return [startingNode.index, secondNodeInPathIndex];
+      }
+
+      this.processConnectedNodes_(currentNode, queue);
     }
+    throw new Error("Could not find a gateway");
+  }
 
-    addLink(node1: number, node2: number) {
-        this.nodes_[node1].addConnectedNode(this.nodes_[node2])
-        this.nodes_[node2].addConnectedNode(this.nodes_[node1])
-        
+  private cleanGraph_() {
+    this.nodes_.forEach(node => {
+      node.isMarked = false;
+      node.previousNode = undefined;
+    });
+  }
+
+  private recursePath_(node: Node, path: number[] = []): number[] {
+    if (!node.previousNode) {
+      path.push(node.index);
+      return path;
     }
+    path.push(node.index);
+    return this.recursePath_(node.previousNode, path);
+  }
 
-    getNode(index: number) {
-        return this.nodes_[index]
+  private processConnectedNodes_(currentNode: Node, queue: Node[]) {
+    for (const connectedNode of currentNode.getConnectNodes()) {
+      if (!connectedNode.isMarked) {
+        connectedNode.previousNode = currentNode;
+        queue.push(connectedNode);
+        connectedNode.isMarked = true;
+      }
     }
-
-    removeLink(node1: number, node2: number) {
-        this.nodes_[node1].removeConnectedNode(this.nodes_[node2])
-        this.nodes_[node2].removeConnectedNode(this.nodes_[node1])
-    }
-
-    findBestLinkToRemove(agentsNodeIndex: number): [number, number] {
-        this.cleanGraph_()
-
-        const startingNode = this.nodes_[agentsNodeIndex]
-        const queue: Node[] = [startingNode]
-        startingNode.isMarked = true
-        while(queue.length > 0) {
-            const currentNode = queue.shift() as Node
-            if (currentNode.isGateway) {
-                const path = this.recursePath_(currentNode)
-                const secondNodeInPathIndex = path[path.length - 2]
-                return [startingNode.index, secondNodeInPathIndex]
-            }            
-
-            for (const connectedNode of currentNode.getConnectNodes()) {
-                if (!connectedNode.isMarked) {
-                    connectedNode.previousNode = currentNode
-                    queue.push(connectedNode)
-                    connectedNode.isMarked = true
-                }
-            }
-        }
-        return [-1, -1] // throw new Error('Could not find a gateway')
-    }
-
-    private cleanGraph_() {
-        this.nodes_.forEach(node => {
-            node.isMarked = false
-            node.previousNode = undefined
-        })
-    }
-
-    private recursePath_(node: Node, path: number[] = []): number[] {
-        if (!node.previousNode) {
-            path.push(node.index)
-            return path
-        }
-        path.push(node.index)
-        return this.recursePath_(node.previousNode, path)
-    }
-
+  }
 }
-
-/*
-const graph = new Graph(3) //?
-graph.getNode(2).isGateway = true
-
-graph.addLink(1, 2)
-graph.addLink(1, 0)
-
-
-graph.findBestLinkToRemove(1) // ?
-*/
--- skynet-revolution-episode-1/src/main.ts
+++ skynet-revolution-episode-1/src/main.ts
@@ -1,31 +1,30 @@
 import { Graph } from "./graph";
 
-declare function readline(): string
+declare function readline(): string;
 
-var inputs = readline().split(' ');
+var inputs = readline().split(" ");
 const N = parseInt(inputs[0]); // the total number of nodes in the level, including the gateways
 
-const network = new Graph(N)
-
+const network = new Graph(N);
 
 const L = parseInt(inputs[1]); // the number of links
 const E = parseInt(inputs[2]); // the number of exit gateways
 for (let i = 0; i < L; i++) {
-    var inputs = readline().split(' ');
-    const N1 = parseInt(inputs[0]); // N1 and N2 defines a link between these nodes
-    const N2 = parseInt(inputs[1]);
-    network.addLink(N1, N2)
+  var inputs = readline().split(" ");
+  const N1 = parseInt(inputs[0]); // N1 and N2 defines a link between these nodes
+  const N2 = parseInt(inputs[1]);
+  network.addLink(N1, N2);
 }
 
 for (let i = 0; i < E; i++) {
-    const EI = parseInt(readline()); // the index of a gateway node
-    network.getNode(EI).isGateway = true
+  const EI = parseInt(readline()); // the index of a gateway node
+  network.getNode(EI).isGateway = true;
 }
 
 // game loop
 while (true) {
-    const SI = parseInt(readline()); // The index of the node on which the Skynet agent is positioned this turn
-    const linkToRemove = network.findBestLinkToRemove(SI)
-    network.removeLink(linkToRemove[0], linkToRemove[1])
-    console.log(`${linkToRemove[0]} ${linkToRemove[1]}`)
+  const SI = parseInt(readline()); // The index of the node on which the Skynet agent is positioned this turn
+  const linkToRemove = network.findBestLinkToRemove(SI);
+  network.removeLink(linkToRemove[0], linkToRemove[1]);
+  console.log(`${linkToRemove[0]} ${linkToRemove[1]}`);
 }
