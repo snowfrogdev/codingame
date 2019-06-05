7be64bd7576be1bcac34262a15ade54d4eead31f
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-06-05 11:03:47 -0400

    Pass first 3 tests

--- closest-number/src/find-closest-number.test.ts
+++ closest-number/src/find-closest-number.test.ts
@@ -1,58 +1,90 @@
-import { findClosestNumber } from "./find-closest-number";
+import {
+  findClosestNumber,
+  getHigherNumber,
+  getLowerNumber
+} from "./find-closest-number";
 
 describe("closest number", () => {
-  test("M has fewer digits", () => {
-    const N = "94754";
-    const M = "3841";
+  describe("codingame tests", () => {
+    test("M has fewer digits", () => {
+      const N = "94754";
+      const M = "3841";
 
-    const expected = findClosestNumber(N, M);
+      const expected = findClosestNumber(N, M);
 
-    const actual = "8431";
+      const actual = "8431";
 
-    expect(expected).toBe(actual)
+      expect(expected).toBe(actual);
+    });
+
+    test("M has more digits", () => {
+      const N = "2749";
+      const M = "284512";
+
+      const expected = findClosestNumber(N, M);
+
+      const actual = "122458";
+
+      expect(expected).toBe(actual);
+    });
+
+    test("Exact solution", () => {
+      const N = "123";
+      const M = "231";
+
+      const expected = findClosestNumber(N, M);
+
+      const actual = "123";
+
+      expect(expected).toBe(actual);
+    });
+
+    test("Solution with leading zeros", () => {
+      const N = "111";
+      const M = "3204";
+
+      const expected = findClosestNumber(N, M);
+
+      const actual = "234";
+
+      expect(expected).toBe(actual);
+    });
+
+    test("Same number of digits, solution < N", () => {
+      const N = "45284";
+      const M = "17404";
+
+      const expected = findClosestNumber(N, M);
+
+      const actual = "44710";
+
+      expect(expected).toBe(actual);
+    });
   });
 
-  test("M has more digits", () => {
-    const N = "2749";
-    const M = "284512";
+  describe("personal tests", () => {
+    test("getHigherNumber", () => {
+      const N = "45284";
+      const M = "17404";
 
-    const expected = findClosestNumber(N, M);
+      const resultArray = M.split("").sort((a, b) => Number(b) - Number(a));
+      const actual = getHigherNumber("4", resultArray);
 
-    const actual = "122458";
+      const expected = "70144";
 
-    expect(expected).toBe(actual);
-  });
+      expect(actual).toBe(expected);
+    });
 
-  test("Exact solution", () => {
-    const N = "123";
-    const M = "231";
+    test("getLowerNumber", () => {
+      const N = "45284";
+      const M = "17404";
 
-    const expected = findClosestNumber(N, M);
+      const resultArray = M.split("").sort((a, b) => Number(b) - Number(a));
+      const actual = getLowerNumber("4", resultArray);
 
-    const actual = "123";
+      const expected = "17440";
 
-    expect(expected).toBe(actual);
-  });
-
-  test("Solution with leading zeros", () => {
-    const N = "111";
-    const M = "3204";
-
-    const expected = findClosestNumber(N, M);
-
-    const actual = "234";
-
-    expect(expected).toBe(actual);
-  });
-  
-  test("Same number of digits, solution < N", () => {
-    const N = "45284";
-    const M = "17404";
-
-    const expected = findClosestNumber(N, M);
-
-    const actual = "44710";
-
-    expect(expected).toBe(actual);
+      expect(actual).toBe(expected);
+    });
   });
 });
--- closest-number/src/find-closest-number.ts
+++ closest-number/src/find-closest-number.ts
@@ -3,24 +3,16 @@ export function findClosestNumber(N: string, M: string) {
   // N.padStart(maxLength, "0");
   // M.padStart(maxLength, "0");
 
-  let resultArray: string[]
+  let resultArray: string[] = M.split("").sort((a, b) => Number(b) - Number(a));
   if (N.length > M.length) {
-    resultArray = M.split("")
-      .sort((a, b) => Number(b) - Number(a));      
-  } else if(N.length < M.length)
-  {
-    resultArray = M.split("")
-    .sort((a, b) => Number(a) - Number(b));
-  }
-  else
-  {
-    resultArray = [];
-    
-    
+  } else if (N.length < M.length) {
+    resultArray = M.split("").sort((a, b) => Number(a) - Number(b));
+  } else {
+    resultArray = recurse(N, resultArray).split("");
   }
 
-  const x: number | undefined = resultArray.findIndex((x) => x !== "0" );
-  const index: number = x ? x: 0;
+  const x: number | undefined = resultArray.findIndex(x => x !== "0");
+  const index: number = x ? x : 0;
 
   resultArray = resultArray.slice(index);
 
@@ -28,7 +20,42 @@ export function findClosestNumber(N: string, M: string) {
   return result;
 }
 
+function recurse(N: string, resultArray: string[], index = 0): string {
+  if (!N.length) {
+    return "";
+  }
+
+  if (resultArray.includes(N[index])) {
+    return N[index] + recurse(N.slice(index + 1), resultArray, index++); // When recurse returns nextHigher or nextLower we are still addin N[Index] and we shouldn't
+  } else {
+    // compare higher and lower to N to see which one is closest and return that
+    const nextHigherNumber: string = getHigherNumber(N[index], resultArray);
+    const nextLowerNumber: string = getLowerNumber(N[index], resultArray);
+    const BigN = BigInt(N);
+    const HigherDiff = BigInt(nextHigherNumber) - BigN;
+    const LowerDiff = BigN - BigInt(nextLowerNumber);
+    return HigherDiff > LowerDiff ? nextLowerNumber : nextHigherNumber;
+  }
+}
+
+export function getHigherNumber(char: string, resultArray: string[]): string {
+  let workingArray = [...resultArray];
+  const startIndex = workingArray.findIndex(x => x <= char) - 1;
+  const startNumber = workingArray.splice(startIndex, 1)[0];
+  workingArray.sort((a, b) => Number(a) - Number(b));
+  return startNumber + workingArray.join("");
+}
+
+export function getLowerNumber(char: string, resultArray: string[]): string {
+  let workingArray = [...resultArray];
+  const startIndex = workingArray.findIndex(x => x < char);
+  const startNumber = workingArray.splice(startIndex, 1)[0];
+  workingArray.sort((a, b) => Number(b) - Number(a));
+  return startNumber + workingArray.join("");
+}
+
 /*
+
 const N = "45284";
 const M = "17404";
 
@@ -40,6 +67,7 @@ const M = "17404";
 
 // Check if we can find the same digit
   //
+
 
 
 
