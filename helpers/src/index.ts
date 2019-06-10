fcb058e68aebf78cbb12eae5cdeb6527f564a493
Philippe Vaillancourt <philippe.m.vaillancourt@gmail.com>
2019-06-10 08:18:45 -0400

    Make helpers it's own packages

---
+++ helpers/src/index.ts
@@ -0,0 +1,9 @@
+import { Array2D } from './array-2d'
+import { Mathb } from './mathb'
+import { createReadline } from './create-realine'
+
+export const Helpers = {
+  Array2D,
+  Mathb,
+  createReadline
+}
